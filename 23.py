5711ba69f41c5a5b544cb9c9760e547d2aa95dd4
Kershkova <124401535+Kershkova@users.noreply.github.com>
2023-03-16 13:14:58 +0200

    Add files via upload

--- 23.py
+++ 23.py
@@ -1,10 +1,17 @@
 min = int(input("Введіть мінімальне значення лічильника: "))
 max = int(input("Введіть максимальне значення лічильника: "))
+
+while min >= max:
+    max = int(input("Введіть адекватне значення максимуму: "))
+
 current_input = int(input("Введіть поточне значення лічильника: "))
+
 while current_input < min or current_input >= max:
     current_input = int(input("Введіть реальне поточне значення (не менше мінімуму і менше максимуму!): "))
 
 print('\n')
+
+
 class Counter:
     def __init__(self, minimum, maximum, current):
         self.minimum = minimum
@@ -19,6 +26,7 @@ class Counter:
         self.current += 1
         print(f"Поточне значення лічильника: {self.current}")
         return (self.current)
+
 
 my_class = Counter(min, max, current_input)
 
